3e2a1df0e0a89e16b5a46acc2f65713dd2a21ff3
vinij-03 <vineetjana3@gmail.com>
2024-09-19 11:23:19 +0530

    init Vadin-Flow- Spring

--- src/main/java/org/vaadin/example/Application.java
+++ src/main/java/org/vaadin/example/Application.java
@@ -1,7 +1,9 @@
-package com.example.application;
+package org.vaadin.example;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,7 +15,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-@Theme(value = "my-app")
+@PWA(name = "Project Base for Vaadin with Spring", shortName = "Project Base")
+@Theme("my-theme")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
